473d7cce35d2ee88326c59d63275ae9b60a79486
NSK-KSN <shadedknght@gmail.com>
2024-09-28 19:58:04 +0300

    feat: cookie prompt

--- src/modules/PersonalDataProcessing/PersonalDataProcessing.tsx
+++ src/modules/PersonalDataProcessing/PersonalDataProcessing.tsx
@@ -1,14 +1,14 @@
 import { Typography } from 'antd';
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 function PersonalDataProcessingModule() {
     return (
        <>
        <Title level={3}>Privacy Policy and Cookies</Title>
-       ROBBO provides the following Terms of Service (“TOS”) to you, which we may update from time to time without notice to you. By accessing and using the ROBBO website, you accept and agree to be bound by the terms and provision of the TOS.
-
-To make this site work properly, we sometimes place small data files called cookies on your device. Most big websites do this too. A cookie is a small text file that a website saves on your computer or mobile device when you visit the site.
+       <Text>ROBBO provides the following Terms of Service (“TOS”) to you, which we may update from time to time without notice to you. By accessing and using the ROBBO website, you accept and agree to be bound by the terms and provision of the TOS.</Text>
+       <br></br>
+       <Text>To make this site work properly, we sometimes place small data files called cookies on your device. Most big websites do this too. A cookie is a small text file that a website saves on your computer or mobile device when you visit the site.</Text>
        </>
     );
 }
